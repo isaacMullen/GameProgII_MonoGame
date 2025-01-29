bd0a826caca11252297ff3880b44913c23b26d8c
isaacMullen <isaacmullen030@gmail.com>
2025-01-29 10:29:23 -0400

    Initial commit

---
+++ GameProgII_MonoGame/Game1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace GameProgII_MonoGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Player _player;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            _player = new Player();
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+            
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Texture2D playerTexture = Content.Load<Texture2D>("Sprites/tile_0078");
+            Vector2 startingPosition = Vector2.Zero;
+
+            _player.LoadContent(playerTexture, startingPosition);
+        }
+
+                
+        protected override void Update(GameTime gameTime)
+        {            
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            // TODO: Add your update logic here
+            
+
+            _player.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _player.Draw(_spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        
+    }
+}
---
+++ GameProgII_MonoGame/Player.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Reflection.Metadata;
+
+
+namespace GameProgII_MonoGame
+{
+    public class Player
+    {
+        public Texture2D Texture { get; private set; }
+        public Vector2 Position { get; private set; }
+        private float spriteScale = 1f;
+
+        private float jumpTime = 0;
+
+        private float gravityMod = 175f;
+        private float jumpForce = 400f;
+
+        private bool gravity = true;
+
+        public void LoadContent(Texture2D texture, Vector2 position)
+        {
+            Texture = texture;
+            Position = position;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            KeyboardState keyBoardState = Keyboard.GetState();
+            MouseState mouseState = Mouse.GetState();
+
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (keyBoardState.IsKeyDown(Keys.Space))
+            {
+                gravity = false;
+                jumpTime = .25f;
+            }
+            if (keyBoardState.IsKeyDown(Keys.D))
+            {
+                Position += new Vector2(1, 0);
+            }
+            if (keyBoardState.IsKeyDown(Keys.A))
+            {
+                Position += new Vector2(-1, 0);
+            }
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                spriteScale += 0.01f;
+            }
+
+            JumpAndGravity(gameTime);
+        }
+
+        public void JumpAndGravity(GameTime gameTime)
+        {
+            double deltaTime = gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (gravity)
+            {
+                Position += new Vector2(0, gravityMod * (float)deltaTime);
+            }
+            else if (!gravity)
+            {
+                Position -= new Vector2 (0, jumpForce * (float)deltaTime);
+            }
+
+            if (jumpTime <= 0)
+            {
+                gravity = true;
+            }
+
+            if (jumpTime > 0)
+            {
+                jumpTime -= .1f;
+            }
+            //Console.Clear();
+            Console.WriteLine(jumpTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, null, Color.White, 0f, Vector2.Zero, spriteScale, SpriteEffects.None, 0f);
+        }
+    }
+}
